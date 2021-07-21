e4299b55f9d037abee345a2384a888875699213f
vatsal <vatsal.acct01@gmail.com>
2021-07-21 11:21:55 +0530

    build dashboard, add signout button, display username

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -1,8 +1,32 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
+import { Alert, Button, Drawer } from 'rsuite';
+import { useProfile } from '../../context/profile.context';
+import { auth } from '../../misc/firebase';
 
 const Dashboard = () => {
-  return <div>Hello</div>;
+  const { profile } = useProfile();
+  const onSignOut = () => {
+    auth.signOut();
+    Alert.info('Signed out', 4000);
+  };
+  return (
+    <>
+      <Drawer.Header>
+        <Drawer.Title>Dashboard</Drawer.Title>
+      </Drawer.Header>
+
+      <Drawer.Body>
+        <h3>Hello! {profile.name}</h3>
+      </Drawer.Body>
+
+      <Drawer.Footer>
+        <Button block color="red" onClick={onSignOut}>
+          Sign out
+        </Button>
+      </Drawer.Footer>
+    </>
+  );
 };
 
 export default Dashboard;
